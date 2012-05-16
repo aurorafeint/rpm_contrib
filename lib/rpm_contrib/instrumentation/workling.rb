b67a1dfa5dc3358d8a466279dad307f7bcfe1267
Richard Huang <flyerhzm@gmail.com>
2012-05-16 13:56:31 +0800

    it should use add_transaction_tracer for workling instrument

--- lib/rpm_contrib/instrumentation/workling.rb
+++ lib/rpm_contrib/instrumentation/workling.rb
@@ -2,7 +2,7 @@
 #
 DependencyDetection.defer do
   @name = :workling
-  
+
   depends_on do
     defined?(::Workling) and not ::NewRelic::Control.instance['disable_workling']
   end
@@ -18,8 +18,10 @@ DependencyDetection.defer do
 
     ::Workling::Discovery.discovered.each do |clazz|
       (clazz.public_instance_methods - ::Workling::Base.public_instance_methods).each do |method|
-        puts "added method tracer Workling/#{clazz.name}/#{method}"
-        clazz.send(:add_method_tracer, method, "Workling/#{clazz.name}/#{method}", :category => :task)
+        clazz.class_eval do
+          NewRelic::Agent.logger.debug "added method tracer Workling/#{clazz.name}/#{method}"
+          clazz.send(:add_transaction_tracer, method, :category => :task)
+        end
       end
     end
   end
